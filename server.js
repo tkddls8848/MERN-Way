9d215d8cf06d02520d2f73b96718f86ea5d5ed82
PSI <tkddls8848@gmail.com>
2021-10-06 22:55:06 +0900

    write date in mongoDB collection

--- server.js
+++ server.js
@@ -4,20 +4,35 @@ import Express from 'express';
 
 const app = Express();
 app.use(Express.urlencoded({extended: true}));
+app.use(Express.json({extended: true}));
 
 const AuthUrl = mongoUrl;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(AuthUrl, (err, client) => {
 
+    const db = client.db('todoapp');
+
     app.listen(8080, () => {
         console.log("listening port 8080")
-    });
+    }); 
 
-    app.get('/pet', (request, response) => {
-        response.send("petsadfasdf");
-        console.log('pet');
-    });
-})
+    app.post('/posting-todos', (request, response) => {
+
+        try{
+            db.collection('post').insertOne({title: request.body.title, date: request.body.date});
+            console.log(request.body.title, request.body.date);
+        } catch (e){
+            console.log(e);
+        };
+
+        response.send("<h2>posting-todos</h2>");
+    })
+});
+
+app.get('/pet', (request, response) => {
+    response.send("petsadfasdf");
+    console.log('pet');
+});
 
 app.get('/beauty', (request, response) => {
     response.send("This is beauty page");
@@ -29,9 +44,4 @@ app.get('/', (request, response) => {
 
 app.get('/register-todos', (request, response) => {
     response.sendFile(__dirname + '/write.html');
-});
-
-app.post('/posting-todos', (request, response) => {
-    response.send("asdf")
-    console.log(request.body.title);
 });
