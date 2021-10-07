bc7d7d3bc0210a5b895c64f8c7a3bfed7f8f168f
PSI <tkddls8848@gmail.com>
2021-10-07 22:44:50 +0900

    write DB in ejs template

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import Express from 'express';
 const app = Express();
 app.use(Express.urlencoded({extended: true}));
 app.use(Express.json({extended: true}));
+app.set("view engine", "ejs");
 
 const AuthUrl = mongoUrl;
 const MongoClient = require('mongodb').MongoClient;
@@ -18,30 +19,34 @@ MongoClient.connect(AuthUrl, (err, client) => {
 
     app.post('/posting-todos', (request, response) => {
 
-        try{
+        try {
             db.collection('post').insertOne({title: request.body.title, date: request.body.date});
             console.log(request.body.title, request.body.date);
-        } catch (e){
+            response.sendFile(__dirname + "/view/home.html");
+        } catch (e) {
             console.log(e);
         };
 
-        response.send("<h2>posting-todos</h2>");
+    });
+
+    app.get('/lists', (request, response) => {
+
+        try {
+            db.collection('post').find().toArray((err, result) => {
+                console.log(result);
+                response.render(__dirname + "/view/list.ejs", {posts : result});
+            });
+        } catch (e) {
+            console.log('e' + e);
+        }
+
     })
 });
 
-app.get('/pet', (request, response) => {
-    response.send("petsadfasdf");
-    console.log('pet');
-});
-
-app.get('/beauty', (request, response) => {
-    response.send("This is beauty page");
-});
-
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/view/home.html");
 });
 
 app.get('/register-todos', (request, response) => {
-    response.sendFile(__dirname + '/write.html');
+    response.sendFile(__dirname + "/view/write.html");
 });
