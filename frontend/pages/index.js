25212c30bfecf0422c8db44168b75ba81afb252d
PSI <tkddls8848@gmail.com>
2021-11-02 23:08:47 +0900

    little modification

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,9 @@
 import HeadInfo from '../components/HeadInfo'
-import Nav from '../components/nav'
 
 const Home = () => {
   return (
     <div>
       <HeadInfo title={'My Page'}/>
-      <Nav/>
       <h1>Welcome to the Jungle</h1>
     </div>
   )
--- frontend/pages/photos.js
+++ frontend/pages/photos.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import Nav from '../components/nav';
 import HeadInfo from '../components/HeadInfo';
 
 const photos = () => {
     return (
         <div>
-            <HeadInfo title={'My Photo'}/>
-            <Nav/>
-            <h1>PHOTO PAGE</h1>
+          <HeadInfo title={'My Photo'}/>
+          <h1>Photo Page</h1>
         </div>
-    );
+    )
 };
 
 export default photos;
